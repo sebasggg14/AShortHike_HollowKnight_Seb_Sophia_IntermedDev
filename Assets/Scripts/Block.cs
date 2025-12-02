0bc4ceab9511cb06c302cff3d8e773f48231d90a
s-m-wang <smw9999@nyu.edu>
2025-12-02 00:24:40 -0500

    Fixed Sticky Wall Issue + More
    - Player can jump against walls
    - Player can attack blocks to destroy them in like Hollow Knight
    - Player Stick
    - Overhauled Player movement to be more fluid and smooth

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        GetComponent<Renderer>().material.color = new Color(0, 255, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    void OnTriggerStay (Collider collider)
+    {
+        if (collider.CompareTag("Stick") && Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Destroyed block");
+            Destroy(gameObject);
+        }
+
+    }
+}
